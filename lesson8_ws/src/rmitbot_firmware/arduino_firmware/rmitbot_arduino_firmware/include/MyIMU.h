95d7be20be1a14dabade072040e2f0d501d226d7
suneruuu <ngao021003@gmail.com>
2025-12-31 11:00:53 +0000

    fix: fix pin mapping for esp32

--- lesson8_ws/src/rmitbot_firmware/arduino_firmware/rmitbot_arduino_firmware/include/MyIMU.h
+++ lesson8_ws/src/rmitbot_firmware/arduino_firmware/rmitbot_arduino_firmware/include/MyIMU.h
@@ -4,7 +4,7 @@
 #include <Arduino.h> // Arduino library for basic functions
 #include "ICM_20948.h"
 
-#define I2C_SDA 23 // Pin used on ESP32 for I2C SDA
+#define I2C_SDA 21 // Pin used on ESP32 for I2C SDA
 #define I2C_SCL 22 // Pin used on ESP32 for I2C SCL
 
 void IMUBegin();     // Function to initialize the IMU
